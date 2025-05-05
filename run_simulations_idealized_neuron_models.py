2d51315c0047c6bee9d4245ce29b41a25e127bbe
SusanneDahle <150791607+SusanneDahle@users.noreply.github.com>
2025-05-05 17:12:25 +0200

    Update run_simulations_idealized_neuron_models.py

--- run_simulations_idealized_neuron_models.py
+++ run_simulations_idealized_neuron_models.py
@@ -23,80 +23,6 @@ import brainsignals.neural_simulations as ns
 from brainsignals.plotting_convention import mark_subplots
 import csv
 
-# Real neuron model
-def return_BBP_neuron(cell_name, tstop, dt):
-
-    # load some required neuron-interface files
-    neuron.h.load_file("stdrun.hoc")
-    neuron.h.load_file("import3d.hoc")
-
-    CWD = os.getcwd()
-    cell_folder = join(join(bbp_folder, cell_name))
-    if not os.path.isdir(cell_folder):
-        ns.download_BBP_model(cell_name)
-
-    neuron.load_mechanisms(bbp_mod_folder)
-    os.chdir(cell_folder)
-    add_synapses = False
-    # get the template name
-    f = open("template.hoc", 'r')
-    templatename = ns.get_templatename(f)
-    f.close()
-
-    # get biophys template name
-    f = open("biophysics.hoc", 'r')
-    biophysics = ns.get_templatename(f)
-    f.close()
-
-    # get morphology template name
-    f = open("morphology.hoc", 'r')
-    morphology = ns.get_templatename(f)
-    f.close()
-
-    # get synapses template name
-    f = open(ns.posixpth(os.path.join("synapses", "synapses.hoc")), 'r')
-    synapses = ns.get_templatename(f)
-    f.close()
-
-    neuron.h.load_file('constants.hoc')
-
-    if not hasattr(neuron.h, morphology):
-        """Create the cell model"""
-        # Load morphology
-        neuron.h.load_file(1, "morphology.hoc")
-    if not hasattr(neuron.h, biophysics):
-        # Load biophysics
-        neuron.h.load_file(1, "biophysics.hoc")
-    if not hasattr(neuron.h, synapses):
-        # load synapses
-        neuron.h.load_file(1, ns.posixpth(os.path.join('synapses', 'synapses.hoc')
-                                       ))
-    if not hasattr(neuron.h, templatename):
-        # Load main cell template
-        neuron.h.load_file(1, "template.hoc")
-
-    templatefile = ns.posixpth(os.path.join(cell_folder, 'template.hoc'))
-
-    morphologyfile = glob(os.path.join('morphology', '*'))[0]
-
-
-    # Instantiate the cell(s) using LFPy
-    cell = LFPy.TemplateCell(morphology=morphologyfile,
-                             templatefile=templatefile,
-                             templatename=templatename,
-                             templateargs=1 if add_synapses else 0,
-                             tstop=tstop,
-                             dt=dt,
-                             lambda_f = 500,
-                             nsegs_method='lambda_f',
-                             v_init = -65)
-    os.chdir(CWD)
-    # set view as in most other examples
-    cell.set_rotation(x=np.pi / 2)
-    return cell
-
-
-
 # Simplified neuron models ----
 def return_ball_and_stick_cell(tstop, dt, apic_diam=2):
 
@@ -800,127 +726,22 @@ def run_simulation_neuron_models(freq, neurons, diam, upper_len, tstop, dt, cuto
                 print(f"{f} Hz complete for Two Compartment model")
 
 
-        else:
-            ns.compile_bbp_mechanisms(cell_name)
-
-            for f in freq:
-                if check_existing_data(amp_data_file_path, cell_name, f):
-                    print(f"Skipping {cell_name} at {f} Hz (already exists in data)")
-                    continue
-                
-                cell = return_BBP_neuron(cell_name, tstop, dt)
-                ns.remove_active_mechanisms(remove_list, cell)
-                
-                cell.extracellular = True
-
-                for sec in cell.allseclist:
-                    sec.insert("extracellular")
-
-                # Calculate and insert extracellular potential
-                base_pot = local_ext_pot(
-                    cell.x.mean(axis=-1),
-                    cell.y.mean(axis=-1),
-                    cell.z.mean(axis=-1)
-                ).reshape(cell.totnsegs, 1)
-
-                pulse = np.sin(2 * np.pi * f * t_ / 1000)
-
-                v_cell_ext = np.zeros((cell.totnsegs, n_tsteps_))
-                v_cell_ext = base_pot * pulse.reshape(1, n_tsteps_)
-
-                cell.insert_v_ext(v_cell_ext, t_)
-                cell.simulate(rec_vmem=True)
-
-                # Calculate soma amp with fourier
-                cut_tvec = cell.tvec[cell.tvec > 2000]
-                cut_soma_vmem = cell.vmem[0, cell.tvec > 2000]
-                freqs, vmem_amps = ns.return_freq_and_amplitude(cut_tvec, cut_soma_vmem)
-                freq_idx = np.argmin(np.abs(freqs - f))
-                soma_amp = vmem_amps[0, freq_idx]
-
-                # Write data to .npy file
-                if cell_name not in amp_data:
-                    amp_data[cell_name] = {
-                        'freq': [],
-                        'soma_vmem_amp': []
-                    }
-                amp_data[cell_name]['freq'].append(f)
-                amp_data[cell_name]['soma_vmem_amp'].append(soma_amp)
-
-                # Save amp data to .npy file
-                np.save(amp_data_file_path, amp_data)
-                print(f"Amplitude data has been saved to {os.path.abspath(amp_data_file_path)}")
-
-                if f in [10, 100, 1000]:
-                    # Calculate amplitude in all segments by removing the part before the cell stabilizes 
-                    vmem_amplitudes = []
-                    for idx in range(cell.totnsegs):
-                        cut_tvec = cell.tvec[cell.tvec > 2000]
-                        cut_vmem = cell.vmem[idx, cell.tvec > 2000]
-                        freqs, vmem_amps = ns.return_freq_and_amplitude(cut_tvec, cut_vmem)
-                        freq_idx = np.argmin(np.abs(freqs - f))
-                        amplitude = vmem_amps[0, freq_idx]
-                        vmem_amplitudes.append(amplitude)
-
-                    # Save plot-data in a .npy file
-                    if cell_name not in plot_data:
-                        plot_data[cell_name] = {
-                            'freq': [],
-                            "x": cell.x.tolist(),
-                            "z": cell.z.tolist(),
-                            "totnsegs": cell.totnsegs,
-                            "tvec": cell.tvec.tolist(),
-                            "soma_vmem": [],
-                            "vmem_amps": []
-                        }
-
-                    plot_data[cell_name]['freq'].append(f)
-                    plot_data[cell_name]['soma_vmem'].append(cell.vmem[0].tolist())
-                    plot_data[cell_name]['vmem_amps'].append(vmem_amplitudes)
-                    
-                    np.save(plot_data_file_path, plot_data)
-                    print(f"Plot data for selected frequency: {f} Hz has been saved to {os.path.abspath(plot_data_file_path)}")
-                    
-                del cell
-
-                print(f"{f} Hz complete for {cell_name}")
-
-
 if __name__=='__main__':
     ns.load_mechs_from_folder(ns.cell_models_folder)
 
-    h = neuron.h
-
-    real_cells_folder = '/Users/susannedahle/Python/brainsignals/cell_models/bbp_models'
-    bbp_folder = os.path.abspath(real_cells_folder)                              # Make this the bbp_folder
-
-    cell_models_folder = '/Users/susannedahle/Python/brainsignals/cell_models'
-    bbp_mod_folder = join(cell_models_folder, "bbp_mod")                        # Mappen med ulike parametere og mekanismer 
+    h = neuron.h          
 
     cell_title_dict = {"Stick": "Stick neuron",
                     "BnS": "Ball-and-Stick",
                     "Bn2S": "Ball-and-Two-Sticks",
-                    "TwoComp": "Two compartments model",
-                    "L5_MC_bAC217_1": "L5 martinotti cell",
-                    "L5_TTPC2_cADpyr232_2": "L5 pyramidal cell",
-                    "L5_NGC_bNAC219_5": "L5 neurogliaform cell"}
+                    "TwoComp": "Two compartments model"}
 
     # One pyramidal cell and two interneurons from L5
     neurons = ["Stick",
             "BnS",
             "Bn2S",
-            "TwoComp", 
-            "L5_TTPC2_cADpyr232_2",
-            "L5_MC_bAC217_1",
-            "L5_NGC_bNAC219_5"
-            ]
+            "TwoComp"]
     
-    # List of active mechanisms to remove to make passive
-    remove_list = ["Ca_HVA", "Ca_LVAst", "Ca", "CaDynamics_E2", 
-                "Ih", "Im", "K_Pst", "K_Tst", "KdShu2007", "Nap_Et2",
-                "NaTa_t", "NaTs2_t", "SK_E2", "SKv3_1", "StochKv"]
-
-
     # Simulation time 
     tstop = 5000.
     dt = 2**-4
